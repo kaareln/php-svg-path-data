2f2379f6445524990bfc16c12d436b4f474585b6
kaareln <64361697+kaareln@users.noreply.github.com>
2023-03-27 11:47:43 +0200

    Create PathDataCommandInterface.php

---
+++ src/Attributes/PathData/PathDataCommandInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SVG\Attributes\PathData;
+
+interface PathDataCommandInterface
+{
+    /**
+     * Gets the name of this class can handle
+     *
+     * @return string[]
+     */
+    public static function getNames(): array;
+
+    /**
+     * Gets the name of the current instruction
+     */
+    public function getName(): string;
+
+    public function requiresPrevious(): bool;
+
+    public function getPrevious(): ?PathDataCommandInterface;
+
+    public function setPrevious(?PathDataCommandInterface $previousInstruction): static;
+
+    public function __toString(): string;
+
+    /**
+     * @return array{0: float, 1: float}  Array of coordinates of all used points
+     */
+    public function getPoints(): array;
+
+    /**
+     * @return array{0: float, 1: float}[]  Absolute coordinates of last point
+     */
+    public function getLastPoint(): array;
+}
